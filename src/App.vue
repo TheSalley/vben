37e28731445a124024a1c48a269c962bfeead0cf
theshire <1919532973@qq.com>
2023-07-26 21:39:34 +0800

    vben 初始化

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>vben</h1>
+  <ConfigProvider> </ConfigProvider>
+</template>
+<script lang="ts" setup>
+import { ConfigProvider } from "ant-design-vue";
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "static/js/title_database/[name]-[hash].js",
+        entryFileNames: "static/js/title_database/[name]-[hash].js",
+        assetFileNames: "static/[ext]/title_database/[name]-[hash].[ext]",
+      },
+    },
+  },
+  resolve: {},
+  css: {
+    preprocessorOptions: {},
+  },
+  plugins: [Vue()],
+  server: {
+    proxy: {
+      "/api": {
+        // target: "http://192.168.2.18",
+        target: "https://dev.iweiqidian.com",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
